1317f09a8fb2ca8ab23865cb8d2d5ca3358738d1
Thulile18 <busisiwegqada0@gmail.com>
2026-06-09 06:21:32 +0200

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,11 @@
+document
+.getElementById("signupForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Account created successfully!");
+
+    window.location.href = "index.html";
+
+});
