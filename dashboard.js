bd400b7e827e5e2eeffce52ab9d568ba06846fb2
Thulile18 <busisiwegqada0@gmail.com>
2026-06-09 06:30:15 +0200

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,12 @@
+document
+.querySelector(".logout-btn")
+.addEventListener("click", () => {
+
+    const confirmLogout =
+        confirm("Are you sure you want to logout?");
+
+    if(confirmLogout){
+        window.location.href = "login.html";
+    }
+
+});
